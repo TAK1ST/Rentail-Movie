f07a9d49bb36ca2be790a0236794c39baeeeafbc
KIETTA-TAK <90909909+TAK1ST@users.noreply.github.com>
2024-10-26 02:17:04 +0700

    movierentalsystemdb.sql
    Remove wishlist, genre, Category,Review
    change genre_id INT to  to genre_name Nvarchar(100)

--- movierentalsystemdb.sql
+++ movierentalsystemdb.sql
@@ -1,45 +1,54 @@
 -- drop schema movierentalsystemdb;
--- create schema movierentalsystemdb;-- 
-USE movierentalsystemdb;
-Create table Users
-(
-	user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username NVARCHAR(50) NOT NULL ,
-    password_hash VARCHAR(255) NOT NULL ,
-    email VARCHAR(100) NOT NULL ,
--- role use integer help flexible and can expand more. 
-    role INTEGER NOT NULL,		
-    subscription_id INT,
-    FOREIGN KEY (subscription_id) REFERENCES Subscription(subscription_id) 
-);
+-- create schema movierentalsystemdb;
+-- USE movierentalsystemdb;
 
-CREATE TABLE Subscription
-(
-	subscription_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Subscription (
+    subscription_id INT AUTO_INCREMENT PRIMARY KEY,
     subsciption_type INTEGER NOT NULL,
     start_date datetime,
     end_date datetime
 );
 
-CREATE TABLE Genre
-(
-	genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    genre_name varchar(100) NOT NULL
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username NVARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    role INTEGER NOT NULL,
+    subscription_id INT,
+    FOREIGN KEY (subscription_id) REFERENCES Subscription(subscription_id)
 );
+
+CREATE TABLE Login_History (
+    login_history_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    login_time DATETIME NOT NULL,
+    logout_time DATETIME,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Customer_Profile (
+    profile_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    full_name VARCHAR(255),
+    address VARCHAR(255),
+    phone_number VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
 
 CREATE TABLE Movie
 (
 	movie_id INT AUTO_INCREMENT PRIMARY KEY,
     title nvarchar(100) NOT NULL,
 	description TEXT,
-    genre_id INT NOT NULL,
+    genre_name NVARCHAR(100) NOT NULL,
     rating ENUM('G', 'PG', 'PG-13', 'R', 'NC-17', 'NR'),
     language varchar(20),
     release_year date,
     rental_price DECIMAL(10,2) NOT NULL,
     -- track the number of available copies for rent
-    available_copies INT DEFAULT 1 ,
-    FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
+    available_copies INT DEFAULT 1
 );
 -- American rating standard 
 -- G: General Audiences. Suitable for all ages. Contains no content that could be considered inappropriate for children.
@@ -82,49 +91,36 @@ CREATE TABLE Movie_Actor (
 
 CREATE TABLE Payment (
     payment_id INT AUTO_INCREMENT PRIMARY KEY,
-    rental_id INT,
-    payment_date DATE NOT NULL,
-    amount DECIMAL(5, 2) NOT NULL,
+    rental_id INT, 
+    payment_date DATE NOT NULL, -- date rental
+    amount DECIMAL(5, 2) NOT NULL, -- price of rental
     FOREIGN KEY (rental_id) REFERENCES Rental(rental_id)
 );
 
-CREATE TABLE Customer_Profile (
-    profile_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    full_name VARCHAR(255),
-    address VARCHAR(255),
-    phone_number VARCHAR(20),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
-);
 
-CREATE TABLE Login_History (
-    login_history_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    login_time DATETIME NOT NULL,
-    logout_time DATETIME,
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
-);
 
-CREATE TABLE Review (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    user_id INT,
-    review_text TEXT,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-    review_date DATE,
-    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
-);
 
-CREATE TABLE Wishlist (
-    wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    movie_id INT,
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
-    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
-);
+-- CREATE TABLE Review (
+--     review_id INT AUTO_INCREMENT PRIMARY KEY,
+--     movie_id INT,
+--     user_id INT,
+--     review_text TEXT,
+--     rating INT CHECK (rating BETWEEN 1 AND 5),
+--     review_date DATE,
+--     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+--     FOREIGN KEY (user_id) REFERENCES User(user_id)
+-- );
 
-CREATE TABLE Category (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(100) NOT NULL
-);
+-- CREATE TABLE Wishlist (
+--     wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
+--     user_id INT,
+--     movie_id INT,
+--     FOREIGN KEY (user_id) REFERENCES User(user_id),
+--     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+-- );
+
+-- CREATE TABLE Category (
+--     category_id INT AUTO_INCREMENT PRIMARY KEY,
+--     category_name VARCHAR(100) NOT NULL
+-- );
+
