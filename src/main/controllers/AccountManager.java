c7aaf8e401ff45defec61c0f0c99646e81dab24b
TAK1ST <trananhkiet21082005@gmail.com>
2024-11-30 00:47:54 +0700

    en&de scriptPassword

--- src/main/controllers/AccountManager.java
+++ src/main/controllers/AccountManager.java
@@ -65,7 +65,7 @@ public class AccountManager extends ListManager<Account> {
         list.add(new Account(
                 id,
                 username,
-                password,
+                encryptPassword(password),
                 email,
                 AccRole.CUSTOMER,
                 AccStatus.OFF));
--- src/main/services/AuthenServices.java
+++ src/main/services/AuthenServices.java
@@ -1,34 +1,66 @@
 package main.services;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static main.config.Database.getConnection;
 import static main.controllers.Managers.getACM;
 import main.dto.Account;
 import static main.utils.Input.getString;
 import static main.utils.LogMessage.errorLog;
 import main.utils.Menu;
+import main.constants.AccRole;
+import main.constants.AccStatus;
+import static main.utils.PassEncryptor.validatePassword;
 
-/**
- *
- * @author trann
- */
 public class AuthenServices {
-    
-    public static Account login() {
+
+    public static Account login() throws SQLException {
         Account account = null;
 
         Menu.showTitle("Login");
         String input = getString("Enter username or email", false);
         String password = getString("Enter password", false);
 
-        for (Account item : getACM().getList()) 
-            if(item.getUsername().equals(input) || item.getEmail().equals(input))
-                if (item.getPassword().equals(password)) {
-                    account = new Account(item);
-                    break;
-                }                
+        // connect database 
+        Connection connection = getConnection();
 
-        if(account == null)
-            errorLog("Wrong username or pasword");
+        try {
+            String query = "SELECT * FROM accounts WHERE username = ? OR email = ?";
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, input);  
+            stmt.setString(2, input);  
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String storedPassword = rs.getString("password");
+
+                if (validatePassword(password,storedPassword)) {
+                    String id = rs.getString("id");
+                    String username = rs.getString("username");
+                    String email = rs.getString("email");
+                    AccRole role = AccRole.valueOf(rs.getString("role"));
+                    AccStatus status = AccStatus.valueOf(rs.getString("status"));
+                    account = new Account(id, username, storedPassword, email, role, status);
+                } else {
+                    errorLog("Wrong username or password");
+                }
+            } else {
+                errorLog("User not found");
+            }
+        } catch (SQLException e) {
+            errorLog("Database error: " + e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();  
+                }
+            } catch (SQLException e) {
+                errorLog("Error closing database connection: " + e.getMessage());
+            }
+        }
 
         return account;
     }
@@ -36,25 +68,25 @@ public class AuthenServices {
     public static Account registor() throws IOException {
         boolean checkCreate = true;
 
-        Menu.showTitle("Registor");
-        String[] options = { "Registor new account", "Back" };
+        Menu.showTitle("Register");
+        String[] options = {"Register new account", "Back"};
         Menu.showOptions(options, 1);
         int input = Menu.getChoice("Enter choice", options.length);
-        switch(input) {
-            case 1: 
+        switch (input) {
+            case 1:
                 checkCreate = checkCreate && getACM().registorAccount();
                 break;
-            case 2: 
+            case 2:
                 return null;
         }
 
-        if(!checkCreate) {
-            errorLog("Can not registor");
+        if (!checkCreate) {
+            errorLog("Cannot register account");
             return null;
-        } 
-        else System.out.println("Registor success!!");
+        } else {
+            System.out.println("Registration successful!");
+        }
 
         return getACM().getList().getLast();
     }
-  
 }
